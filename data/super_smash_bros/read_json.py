01d691c92dd8dc6e3f37c916b272f86fae936616
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-11 23:17:38 -0500

    done exersice one

--- data/super_smash_bros/read_json.py
+++ data/super_smash_bros/read_json.py
@@ -28,10 +28,58 @@ def read_all_json_files(json_file):
 # then be saved in a list with the read_all_json_files function
 read_all_json_files("mario.json")
 read_all_json_files("link.json")
-
+yoshi = read_all_json_files("yoshi.json")
 
 # Iterating through the list that is holding the json files
 # in a dictionary
 for j in json_list:
     for key, value in j.items():
         print(key, value)
+
+
+# The "json" file path being used for the write_pickle function
+captain_falcon = {
+    "name": "Captain Falcon",
+    "neutral_special": "Falcon Punch",
+    "side_special": "Raptor Boost",
+    "up_special": "Falcon Dive",
+    "down_special": "Falcon Kick",
+    "final_smash": "Blue Falcon"
+}
+def write_pickle(file_name):
+    """
+    This function takes in a file path as a parameter and the
+    function writes the contents of the json file to a file
+    called super_smash_characters.pickle
+    :param file_name:
+    :return:
+    """
+    with open("super_smash_characters.pickle", "w") as f:
+        character = json.dump(file_name, f)
+        return character
+
+
+def load_pickle(character_data):
+    """
+    When given a file path as the parameter, the function will
+    open up a pickled file and return the data
+    :param character_data:
+    :return:
+    """
+    with open(character_data) as f:
+        character = json.load(f)
+    return character
+
+# I passed the captain_falcon dict (json file) into the function
+# write pickle so that it can save this json file in it
+captain_falcon_json = write_pickle(captain_falcon)
+
+# This is taking the pickled file and loading it to be
+# printed to the console. It has the file I wrote into
+# it and will return that data. saved to a
+# variable, so I can iterate through it
+cp_fal = load_pickle("super_smash_characters.pickle")
+
+# Iterates through the file and prints to the console
+for key, value in cp_fal.items():
+    print(key, value)
