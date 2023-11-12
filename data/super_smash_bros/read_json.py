c31c07c9e55172ea06c50ad75f65e9ae2df1a0c5
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-11 22:28:24 -0500

    made a function that returns a list of json files passed through

--- data/super_smash_bros/read_json.py
+++ data/super_smash_bros/read_json.py
@@ -1,7 +1,24 @@
 import json
 def read_json():
-    with open("mario.json") as smb:
-        character = json.load(smb)
+    with open("mario.json") as m:
+        character = json.load(m)
     print(character)
 
-read_json()
+
+def read_json2():
+    with open("link.json") as l:
+        character = json.load(l)
+    print(character)
+json_list = []
+def read_all_json_files(json_file):
+    with open(json_file) as f:
+        file_object = json.load(f)
+    json_list.append(file_object)
+
+read_all_json_files("mario.json")
+read_all_json_files("link.json")
+
+for j in json_list:
+    for key, value in j.items():
+        print(key, value)
+# read_json()
